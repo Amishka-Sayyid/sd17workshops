8213092f1fdcc44b1b1f915ed47773664c2bcab9
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-06 12:07:59 +0000

    From Live Server to Vite workshop

---
+++ week4/vite-project/src/main.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+  
+    <h1>Hello Vite!</h1>
+   
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quas cum ipsa libero molestiae fugit, fugiat consequatur beatae enim possimus nisi consequuntur fuga assumenda repudiandae iste cupiditate nulla! Doloribus, suscipit.
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
