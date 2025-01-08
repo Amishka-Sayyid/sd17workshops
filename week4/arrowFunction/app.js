88f4101da568759543ba051bdf99f539f0b9fc05
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-08 21:52:56 +0000

    added arrow function

--- week4/arrowFunction/app.js
+++ week4/arrowFunction/app.js
@@ -1,40 +1,31 @@
-function add(a, b) {
-  return a + b;
-}
-
-add(1, 2);
-
-//arrow function
-const add = (a, b) => {
-  return a + b;
-};
-
-add(1, 2);
-
-//if the function body is a single line you can leave out the curly braces and the return keyword, returning the value directly:
-
-const add = (a, b) => a + b;
-
-add(1, 2);
-
-//Asynchronous Arrow functions
-async function getPosts() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-  return data;
-}
-//⛳️ And here it is written as an “arrow function” expression, where we assign the function to a const, and call it using that name:
-
-const getPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-  return data;
-};
-
-async function main() {
-  const posts = await getPosts();
-  console.log("Posts:", posts);
-}
-main();
-
 //workshop
+//fetch api
+
+const getFoxImage = async () => {
+  const response = await fetch("https://randomfox.ca/floof");
+  console.log(response);
+
+  const data = await response.json();
+
+  const wrangledData = data.image;
+
+  return wrangledData;
+};
+
+const imageContainer = document.getElementById("foxApi");
+
+const CreateImage = (foxUrl) => {
+  const newImage = document.createElement("img");
+
+  newImage.src = foxUrl;
+  newImage.alt = "random fox images";
+  newImage.className = "foxImage";
+
+  imageContainer.appendChild(newImage);
+};
+
+async function display() {
+  const finalImage = await getFoxImage();
+  CreateImage(finalImage);
+}
+display();
