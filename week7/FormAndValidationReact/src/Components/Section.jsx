744f57f866a1a400959dc5a288fa92d0a33937c7
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-29 22:37:39 +0000

    react router routes working well

--- week7/FormAndValidationReact/src/Components/Section.jsx
+++ week7/FormAndValidationReact/src/Components/Section.jsx
@@ -4,7 +4,17 @@ export default function Section() {
   return (
     <>
       <h4>Click on links below for suprise!!!</h4>
-      <div className="links"></div>
+      <div className="links">
+        <Link to={"/"} className="singleLink">
+          Home
+        </Link>
+        <Link to={"/Form1"} className="singleLink">
+          SingleInputForm
+        </Link>
+        <Link to={"/Form2"} className="singleLink">
+          MultipleInputForm
+        </Link>
+      </div>
     </>
   );
 }
