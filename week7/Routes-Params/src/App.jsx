ef82b5c0f5f172a4f0f765b550222687a41b59c7
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-30 15:21:55 +0000

    restarting again

--- week7/Routes-Params/src/App.jsx
+++ week7/Routes-Params/src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { UserPage } from "./components/UserPage";
 
-import { UserPostsPage } from "./components/UserPostsPage";
-import Header from "./components/Header";
-import About from "./components/About";
 export default function App() {
   return (
     <>
-      <Header />
-      <h1>Query Strings, URL Params, Nested Routes</h1>
-
-      <About />
-
-      {/* creating dynamic routes with React router */}
-      <Routes>
-        <Route path="/users/:username" element={<UserPage />}>
-          <Route path="/users/:username/posts" element={<UserPostsPage />} />
-        </Route>
-      </Routes>
+      <h1>App</h1>
     </>
   );
 }
