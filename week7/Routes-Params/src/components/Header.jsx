161d00491a2bf17490630cb5e2afa5194e671ec9
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-31 10:47:19 +0000

    routes done

--- week7/Routes-Params/src/components/Header.jsx
+++ week7/Routes-Params/src/components/Header.jsx
@@ -9,9 +9,6 @@ export default function Header() {
           <button>
             <Link to={"/"}>Home</Link>
           </button>
-          <button>
-            <Link to={"/about"}>About</Link>
-          </button>
         </div>
         <h3>Workshop</h3>
         <div>
--- week7/Routes-Params/src/components/Section.jsx
+++ week7/Routes-Params/src/components/Section.jsx
@@ -1,10 +1,23 @@
 import "./Header.css";
+import { Routes, Route } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import UserComments from "./UserComments";
+import { Link } from "react-router-dom";
+
 export default function Section() {
   return (
     <>
       <div className="section">
         <h1>Query Strings, URL Params, Nested Routes</h1>
-        <h3>content goes here...</h3>
+        <button>
+          <Link to={"users/:id"}>User profile</Link>
+        </button>
+
+        <Routes>
+          <Route element={<UserProfile />} path={"/users/:id"}>
+            <Route element={<UserComments />} path={"/users/:id/:comment"} />
+          </Route>
+        </Routes>
       </div>
     </>
   );
