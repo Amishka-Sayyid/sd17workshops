f013f44f4d0174cd22f0ef0fb4c4c1fe6bc8b355
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-04 15:37:33 +0000

    Next.js Data Fetching

---
+++ week8/03-data-fetching/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import "./Header.css";
+export default function Header() {
+  return (
+    <>
+      <div className="Header">
+        <h1>Next.js Data Fetching</h1>
+        <nav>
+          <button>
+            <Link href={"/"}>Home</Link>
+          </button>
+          <button>
+            <Link href={"/about"}>About</Link>
+          </button>
+          <button>
+            <Link href={"/contact"}>Contact</Link>
+          </button>
+        </nav>
+      </div>
+    </>
+  );
+}
