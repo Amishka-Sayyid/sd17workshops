209fd84116ceb552fd56fa8f67a70feb21b0eb25
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-08 01:34:19 +0000

    header title

--- week8/05-postgress-in-next/src/components/Header.jsx
+++ week8/05-postgress-in-next/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <div className="Header">
-        <h1>Next.js Data Fetching</h1>
+        <h1>postgress -in -next</h1>
         <nav>
           <button>
             <Link href={"/"}>Home</Link>
