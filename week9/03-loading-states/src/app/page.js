bbd469062e74c4d92ad489aa232746694bedfec9
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-11 13:48:09 +0000

    loading States and Suspense

--- week9/03-loading-states/src/app/page.js
+++ week9/03-loading-states/src/app/page.js
@@ -1,20 +1,35 @@
+"use client";
 import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+
+// ActiveLink component
+export function ActiveLink({ href, children }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link href={href} className={isActive ? "text-orange-400" : ""}>
+      {children}
+    </Link>
+  );
+}
 export default function HomePage() {
   return (
     <>
       <div className="flex flex-col items-center justify-center p-10 border  border-gray-300 rounded-lg shadow-lg">
         <h1>Loading States and Suspense workshop!</h1>
 
-        <Link href="/slow">
+        <ActiveLink href="/slow">
           <button className="w-full p-3 mb-4 text-gray-800 rounded-md  bg-slate-200 border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent text-xl">
             slow
           </button>
-        </Link>
-        <Link href="/fast">
+        </ActiveLink>
+        <ActiveLink href="/fast">
           <button className="w-full p-3 mb-4 text-gray-800 rounded-md border-2 border-gray-300 bg-slate-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent text-xl">
             fast
           </button>
-        </Link>
+        </ActiveLink>
       </div>
     </>
   );
--- week9/03-loading-states/src/app/slow/loading.js
+++ week9/03-loading-states/src/app/slow/loading.js
@@ -6,6 +6,7 @@ export default function Loading() {
   return (
     <div className="flex h-screen w-full justify-center pt-20">
       <PacmanLoader loading={true} color="#ffffff" />
+      <h1 className="text-sky-400">Be patient! Content is coming soon...</h1>
     </div>
   );
 }
