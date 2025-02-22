c665faef9f2a880312c6f60d73c48576039b946b
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-22 20:58:09 +0000

    variable intro workshop

---
+++ week1/01-variable-intro/variable.js
@@ -0,0 +1,28 @@
+// JS: Variables Intro
+// Create other variables and and log them to the console. Call them whatever you want.
+
+let myName = "Bob";
+console.log(myName);
+// 🎯 Create multiple numbers and add them together.
+let x = 1;
+let y = 2;
+
+let j = x + y;
+console.log(j);
+
+// 🎯 Create multiple strings and combine them into a complete sentence using “string concatenation”. Log them out with console.log.
+
+let course = "software development";
+let sentense = "i am surrently studying";
+
+console.log(sentense + " " + course);
+
+/*🏹 Try to swap the values of two variables. For example, if you have a variable a with a value of 1 and a variable b with a value of 2, try to swap the values so that a has a value of 2 and b has a value of 1.*/
+
+let a = 1;
+let b = 3;
+
+[a, b] = [b, a];
+
+console.log("a=", a);
+console.log("b=", b);
