0291944686591df604559f324a215e88a75fe863
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-24 11:40:55 +0000

    mdn Active learning: Printing those products

---
+++ week2/arrays/mdnexercise.js
@@ -0,0 +1,28 @@
+const list = document.getElementById("outputul");
+const totalBox = document.getElementById("outputp");
+let total = 0;
+list.textContent = "";
+totalBox.textContent = "";
+
+const products = [
+  "Underpants:6.99",
+  "Socks:5.99",
+  "T-shirt:14.99",
+  "Trousers:31.99",
+  "Shoes:23.99",
+];
+
+for (const product of products) {
+  const subArray = product.split(":");
+  console.log(subArray);
+  const name = subArray[0];
+  const price = Number(subArray[1]);
+  total += price;
+  const itemText = `${name} — $${price}`;
+
+  const listItem = document.createElement("li");
+  listItem.textContent = itemText;
+  list.appendChild(listItem);
+}
+
+totalBox.textContent = `Total: $${total.toFixed(2)}`;
